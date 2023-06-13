edcd0dd86186b12c89432ce5c22f18138581875d
adrian04981 <112140637+adrian04981@users.noreply.github.com>
2023-06-13 13:43:43 -0500

    Update Dashboard-management.vue

--- src/views/Dashboard/Dashboard-management.vue
+++ src/views/Dashboard/Dashboard-management.vue
@@ -1,8 +1,20 @@
 <template>
   <v-row>
-    <v-col cols="12" md="4" lg="32">
+    <v-col v-if="user.rol=='5'" cols="12" md="4">
+      <v-card class="card-container">
+        <div class="card-content">
+          <div class="label">Usuario:</div>
+          <v-label>{{ generatedUser }}</v-label>
+          <div class="label">Contraseña:</div>
+          <v-label>{{ generatedPassword }}</v-label>
+          <div class="message">IMPORTANTE: Anote estos datos de usuario.Serán usados para futuras consultas.</div>
+          <div class="message"></div>
+        </div>
+      </v-card>
+    </v-col>
+    <v-col cols="12" md="8" lg="4">
       <div class="home-columns">
-        <p v-if="user.rol == '4'||user.rol=='5'">
+        <p v-if="user.rol == '4' || user.rol == '5'">
           <CardReserva :user="this.user" />
           <CardPagosPendientes :user="this.user" />
         </p>
@@ -10,11 +22,10 @@
     </v-col>
   </v-row>
 </template>
-<script>
 
+<script>
 import CardReserva from "@/components/CardReserva.vue";
 import CardPagosPendientes from "@/components/CardPagosPendientes.vue";
-//import CardTodo from "@/components/Todo.vue";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
@@ -24,33 +35,62 @@ export default {
     title: "Dashboard",
   },
   components: {
-    CardReserva, CardPagosPendientes,
-
+    CardReserva,
+    CardPagosPendientes,
   },
   data() {
     return {
-
+      generatedUser: '',
+      generatedPassword: ''
     };
   },
   async created() {
     await this.fetchUser();
   },
+  mounted() {
+    this.generateUser();
+  },
   methods: {
     ...mapActions("Authentication", ["fetchUser"]),
+    generateUser() {
+      this.generatedUser = this.user.usuario;
+      this.generatedPassword = this.user.clave;
+    },
   },
   computed: {
     ...mapGetters("Authentication", ["user"]),
   },
 };
 </script>
-<style lang="scss" scoped>
-.home-columns {
 
+<style lang="scss" scoped>
+.card-container {
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  padding: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.card-content {
+  margin-bottom: 20px;
+}
+
+.label {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.message {
+  font-weight: bold;
+  color: #ff5722;
+  margin-bottom: 5px;
+}
+
+.home-columns {
   display: flex;
   flex-direction: row;
-  justify-content: left;
-  flex-wrap: nowrap;
-    margin: 1% 100%;
-
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  margin-top: 20px;
 }
 </style>
